0849f385f2c58c466fe7596e99fcafaa77ff4141
Shion Ichikawa <shion1305@gmail.com>
2024-05-18 15:52:16 +0900

    ✨ sample code for creating new page in notion database

--- scraping.rb
+++ scraping.rb
@@ -1,5 +1,8 @@
 require 'nokogiri'
 require 'open-uri'
+require 'httparty'
+require 'dotenv/load'
+require 'json'
 
 # スクレイピング対象のURL
 url = 'https://www.lycorp.co.jp/ja/recruit/newgrads/internship/engineer/'
@@ -7,10 +10,6 @@ url = 'https://www.lycorp.co.jp/ja/recruit/newgrads/internship/engineer/'
 html = URI.open(url)
 
 doc = Nokogiri::HTML(html)
-
-doc.css('.classname').each do |element|
-  puts element.text
-end
 
 RecruitInfo = Data.define(:type, :title, :url, :salary, :application_deadline, :term)
 
@@ -46,3 +45,62 @@ process_html(
   "Data Scientist", doc, 'body > div.global-container > section.job-course.body-container > div > div:nth-child(9) > ul > li > a', recruit_info)
 
 puts recruit_info
+
+# 環境変数からNotion APIトークンとデータベースIDを取得
+notion_api_token = ENV['NOTION_API_TOKEN']
+database_id = ENV['NOTION_DATABASE_ID']
+
+url = "https://api.notion.com/v1/pages"
+
+headers = {
+  "Authorization" => "Bearer #{notion_api_token}",
+  "Content-Type" => "application/json",
+  "Notion-Version" => "2022-06-28"
+}
+
+page_properties = {
+  "parent" => { "database_id" => database_id },
+  "properties" => {
+    "Name" => {
+      "title" => [
+        {
+          "text" => {
+            "content" => "Tuscan Kale"
+          }
+        }
+      ]
+    },
+  },
+  "children" => [
+    {
+      "object" => "block",
+      "type" => "heading_2",
+      "heading_2" => {
+        "rich_text" => [{ "type" => "text", "text" => { "content" => "Lacinato kale" } }]
+      }
+    },
+    {
+      "object" => "block",
+      "type" => "paragraph",
+      "paragraph" => {
+        "rich_text" => [
+          {
+            "type" => "text",
+            "text" => {
+              "content" => "Lacinato kale is a variety of kale with a long tradition in Italian cuisine, especially that of Tuscany. It is also known as Tuscan kale, Italian kale, dinosaur kale, kale, flat back kale, palm tree kale, or black Tuscan palm.",
+              "link" => { "url" => "https://en.wikipedia.org/wiki/Lacinato_kale" }
+            }
+          }
+        ]
+      }
+    }
+  ]
+}
+
+response = HTTParty.post(url, headers: headers, body: page_properties.to_json)
+
+if response.code == 200
+  puts "ページが正常に追加されました: #{response.body}"
+else
+  puts "エラーが発生しました: #{response.body}"
+end
